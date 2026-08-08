57da994d11ae1ea740dcdbb23877009f84264e2e
kangyujeong7 <jeong4696@gmail.com>
2026-08-09 00:51:10 +0900

    [Passed] 살 수 있는 물건 (51ms, 16MB)

--- trail0/살 수 있는 물건/things-able-to-buy.py
+++ trail0/살 수 있는 물건/things-able-to-buy.py
@@ -1,8 +1,8 @@
 n = int(input())
 
-if (n>=3000):
+if n>=3000:
     print("book")
-elif (n>=1000):
+elif n>=1000: #  3000이상인건 이미 한번거르고 오기 떄문에 쓸필요없다 / 조건 1이 false이면서, 조건2가 True일때 실행할 코드
     print("mask")
 else:
     print("no")
