1edf538079f8a267e6f4c280e5b2c81449e676b6
kangyujeong7 <jeong4696@gmail.com>
2026-08-09 22:07:18 +0900

    [Passed] 정수의 조건 여부 3 (49ms, 16MB)

---
+++ trail0/정수의 조건 여부 3/numbers-condition-3.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+
+if(a%13==0) or (a%19==0):
+    print("True")
+else:
+    print("False")
+
+#     a = int(input())
+
+# # 조건식의 결과(True 또는 False)를 바로 출력합니다.
+# print((a % 13 == 0) or (a % 19 == 0))
