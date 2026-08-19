3dd8e77758bb481e82b22c51828948dc6bc119be
kangyujeong7 <jeong4696@gmail.com>
2026-08-20 00:05:08 +0900

    [Passed] 별 찍는 것을 5번 반복하기 (48ms, 16MB)

---
+++ trail2/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.py
@@ -0,0 +1,16 @@
+# def print_10_stars():
+#         print("*"*10)
+        
+# for _ in range(5):
+#     print_10_stars()
+
+
+
+
+def print_10_stars():
+    for _ in range(10):
+        print("*",end="")
+
+for _ in range(5):
+    print_10_stars()
+    print()
