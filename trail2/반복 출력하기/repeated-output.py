59707a00573d37c9f3795d8fe83410c439f41152
kangyujeong7 <jeong4696@gmail.com>
2026-08-20 20:28:14 +0900

    [Passed] 반복 출력하기 (51ms, 16MB)

---
+++ trail2/반복 출력하기/repeated-output.py
@@ -0,0 +1,16 @@
+
+# def print_n_lines(n):
+#     for _ in range(n):
+#         print("12345^&*()_")
+
+
+# row_num = int(input())
+# print_n_lines(row_num)
+
+N = int(input())
+
+def print_n_lines(n):
+   for i in range(n):
+     print("12345^&*()_")
+
+print_n_lines(N)
